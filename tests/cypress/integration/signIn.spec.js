79e2df61d8553180e4f092261b4d384aaa32840b
Mark Thomas <mpjtho@gmail.com>
2021-05-28 14:11:47 +0100

    allow logout

--- tests/cypress/integration/signIn.spec.js
+++ tests/cypress/integration/signIn.spec.js
@@ -1,8 +1,13 @@
-import { within } from "@testing-library/dom";
-
-describe("Sign in & out", () => {
+describe("Sign in", () => {
   it("should sign in a user successfully", () => {
     cy.signIn();
+
     expect(cy.location("pathname").should("not.include", "login"));
+
+    cy.getCookies()
+      .should("have.length", 1)
+      .then((cookies) => {
+        expect(cookies[0]).to.have.property("name", "session");
+      });
   });
 });
